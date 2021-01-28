0680d5abcdfbfe03d16207fad9990b17a854bdd6
beninvent <77308320+beninvent@users.noreply.github.com>
2021-01-28 11:00:55 -0800

    reworked code

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -4,34 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     Double temperature;
-
     int inputUnits;
     int outputUnits;
 
     EditText Input;
-
     TextView output;
-
-
-    Button convertButton;
 
     Spinner input_Units;
     Spinner output_Units;
 
-
-
-
-
-    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,40 +32,31 @@ public class MainActivity extends AppCompatActivity {
 
         input_Units = findViewById(R.id.input_Units_Spinner);
         output_Units = findViewById(R.id.output_Units_Spinner);
-/*
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.units, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        input_Units.setAdapter(adapter);
-        output_Units.setAdapter(adapter);
-*/
         Input = (EditText) findViewById(R.id.temp_Input);
-
         output = (TextView) findViewById(R.id.temp_Output);
 
-        convertButton = (Button) findViewById(R.id.button);
-        convertButton.setOnClickListener(v -> {
-
-            inputUnits = input_Units.getSelectedItemPosition();
-            outputUnits = output_Units.getSelectedItemPosition();
-
-            output.setText("");
-
-            try {
-                temperature = Double.valueOf(Input.getText().toString());
-
-                //Call Convert method and output the answer
-                String outputString;
-                outputString = String.format("%.2f", Convert(temperature));
-
-                output.setText(outputString);
-            }
-            catch(Exception e) {
-               Toast.makeText(this, "Enter a Number", Toast.LENGTH_SHORT).show();
-            }
-        });
-
     }
+
+    public void convertButtonClick(View view) {
+        inputUnits = input_Units.getSelectedItemPosition();
+        outputUnits = output_Units.getSelectedItemPosition();
+
+        output.setText("");
+
+        try {
+            temperature = Double.valueOf(Input.getText().toString());
+
+            //Call Convert method and output the answer
+            String outputString = String.format("%.2f", Convert(temperature));
+
+            output.setText(outputString);
+        }
+        catch(Exception e) {
+            Toast.makeText(this, "Enter a Number", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     private Double Convert(Double input) {
         double output = 0;
         final double fm = 1.8;
